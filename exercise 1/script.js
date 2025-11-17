3bd6e160a9c1ae291b45585213da1e552ff8319e
cb4yrra <t.chinbayr@gmail.com>
2025-11-17 12:42:32 +0800

    js

---
+++ exercise 1/script.js
@@ -0,0 +1,87 @@
+const x = 5;
+const y = 6;
+const z = 8;
+const l = 10;
+const h = 5;
+
+const P = x + y + z;
+const CP = l * 2 + h * 2;
+const S = l * h;
+
+console.log("Perimetr = " + P);
+
+console.log("Niilber = " + (1 + 3 + 5 + 7 + Math.ceil(8.7)));
+
+console.log("Tegsh Untsugt Perimetr =", CP);
+
+console.log("Talbai =", S);
+
+const second = 98;
+const minute = second / 60;
+const secund = second % 60;
+
+console.log(~~minute, "min", ~~secund, "sec");
+
+const sec = 1297;
+const secunds = sec % 60;
+const minutes = sec / 60;
+const hour = minutes / 60;
+
+console.log(~~hour, "hour", ~~minutes, "min", ~~secunds, "sec");
+
+const minutee = 875;
+const secunde = 88;
+const fullSecunds = minutee * 60 + secunde;
+
+console.log(fullSecunds, "sec");
+
+// let x = 5;
+// let y = 3;
+// let xy = x + y;
+
+// console.log("Bodlogo 1 =", xy);
+
+// const a = 10;
+// const b = 4;
+// const ab = a - b;
+
+// console.log("Bodlogo 2 =", ab);
+
+// const p = 7;
+// const q = 6;
+// const pq = p * q;
+
+// console.log("Bodlogo 3 =", pq);
+
+// const m = 20;
+// const n = 5;
+// const mn = m / n;
+
+// console.log("Bodlogo 4 =", mn);
+
+// x = 15;
+// y = 4;
+// xy = x % y;
+
+// console.log("Bodlogo 5 =", xy);
+
+// let num = 10;
+// num = num + 5;
+
+// console.log("Bodlogo 6 =", num);
+
+// mum = num += 10;
+
+// console.log("Bodlogo 7 =", xum);
+
+// bum = num -= 20;
+
+// console.log("Bodlogo 8 =", bum);
+
+// vum = num *= 3;
+
+// console.log("Bodlogo 9 =", vum);
+
+// zum = num /= 4;
+
+// console.log("Bodlogo 10 =", zum);
