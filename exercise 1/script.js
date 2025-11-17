a99dbd30e792f1509aac168291e79b7beee21d3c
cb4yrra <t.chinbayr@gmail.com>
2025-11-17 19:42:53 +0800

    test

--- exercise 1/script.js
+++ exercise 1/script.js
@@ -12,28 +12,77 @@ console.log("Perimetr = " + P);
 
 console.log("Niilber = " + (1 + 3 + 5 + 7 + Math.ceil(8.7)));
 
-console.log("Tegsh Untsugt Perimetr =", CP);
+console.log("1. ", CP);
 
-console.log("Talbai =", S);
+console.log("2. ", S);
 
 const second = 98;
 const minute = second / 60;
 const secund = second % 60;
 
-console.log(~~minute, "min", ~~secund, "sec");
+console.log("3. ", ~~minute, "min", ~~secund, "sec");
 
 const sec = 1297;
 const secunds = sec % 60;
 const minutes = sec / 60;
 const hour = minutes / 60;
 
-console.log(~~hour, "hour", ~~minutes, "min", ~~secunds, "sec");
+console.log("4. ", ~~hour, "hour", ~~minutes, "min", ~~secunds, "sec");
 
 const minutee = 875;
 const secunde = 88;
 const fullSecunds = minutee * 60 + secunde;
 
-console.log(fullSecunds, "sec");
+console.log("5. ", fullSecunds, "sec");
+
+const hour1 = 83;
+const minute1 = 84;
+const secund1 = 85;
+const answer1 = minute1 * 60;
+const answer2 = answer1 * 60 + secund1;
+
+console.log("6. ", answer2, "sec");
+
+const hour2 = 38;
+const day1 = hour2 / 24;
+const dayhour = hour2 % 24;
+
+console.log("7. ", ~~day1, "day", dayhour, "hour");
+
+const day2 = 4;
+const hour3 = 40;
+const houranswer = day2 * 24 + 40;
+
+console.log("8. ", houranswer, "hour");
+
+const month = 106;
+const year = 106 / 12;
+const yearmonth = 106 % 12;
+
+console.log("9. ", ~~year, "years", yearmonth, "months");
+
+const year2 = 90;
+const month2 = 98;
+const yearmonth2 = 90 * 12 + 98;
+
+console.log("10. ", yearmonth2, "months");
+
+const numbers = [9, 3, 2, 8];
+const minimum = Math.min(...numbers);
+
+console.log("11. ", minimum);
+
+const number = [2, 9, 4, 3];
+const minimum2 = number.filter((number) => number < 5);
+const produce = minimum2.reduce((a, b) => a * b);
+
+console.log("12. ", produce);
+
+const number2 = [11, 9, 4, 3];
+const maximum = number2.filter((num) => num > 5);
+const produces = maximum.reduce((a, b) => a * b);
+
+console.log("13. ", produces);
 
 // let x = 5;
 // let y = 3;
---
+++ exercise 1/script2.js
@@ -0,0 +1,21 @@
+const a = 4;
+const b = 6;
+const P = a * 2 + b * 2;
+const S = a * b;
+const H = P / S;
+
+console.log("Bodlogo-uud", P, S, H);
+
+const r = 3;
+const LR = r * 2;
+const SR = Math.PI * r ** 2;
+
+console.log("Тойргийн талбай:", ~~SR);
+
+const r1 = 3;
+const r2 = 7;
+const SR1 = Math.PI * r1 ** 2;
+const SR2 = Math.PI * r2 ** 2;
+const difference = SR2 - SR1;
+
+console.log("Талбайн ялгаа:", difference);
