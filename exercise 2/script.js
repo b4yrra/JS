5a5b7cac612d3b4c335c3c948b1502e1ecb2bdf8
cb4yrra <t.chinbayr@gmail.com>
2025-11-18 12:43:14 +0800

    test

---
+++ exercise 2/script.js
@@ -0,0 +1,140 @@
+let weekNumber = 3;
+let weekName;
+
+// if (weekNumber === 1) {
+//   weekName = "Davaa";
+// } else if (weekNumber === 2) {
+//   weekName = "Myagmar";
+// } else if (weekNumber === 3) {
+//   weekName = "Lhavga";
+// } else if (weekNumber === 4) {
+//   weekName = "Purev";
+// } else if (weekNumber === 5) {
+//   weekName = "Baasan";
+// }
+
+// if (weekNumber === 6) {
+//   weekName = "Byamba";
+// }
+
+// if (weekNumber === 7) {
+//   weekName = "Nyam";
+// }
+
+// console.log(weekName);
+
+// switch (weekNumber) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+
+//   case 4:
+//     console.log("Thursday");
+//     break;
+
+//   case 5:
+//     console.log("Friday");
+//     break;
+// }
+
+// const numberOne = 12;
+// const numberTwo = 14;
+
+// if (numberOne > numberTwo) {
+//   console.log("numberOne ih", numberOne);
+// } else {
+//   console.log("numberTwo ih", numberTwo);
+// }
+
+// const num = 8;
+// const number = num % 2;
+
+// if (number >= 1) {
+//   console.log("Sondgou Too");
+// } else {
+//   console.log("Tegsh Too");
+// }
+
+// const x = 300;
+// const y = 1000;
+// const z = 6000;
+
+// if (x > y) {
+//   if (x > z) {
+//     console.log("x ih");
+//   } else {
+//     console.log("z ih");
+//   }
+// } else {
+//   if (y > z) {
+//     console.log("y ih");
+//   } else {
+//     console.log("z ih");
+//   }
+// }
+
+// let Name = "Batbileg";
+// let string = Name.length;
+
+// console.log(string);
+
+// let firstName = "Batbileg";
+// let uppercase = firstName.toUpperCase();
+
+// console.log(uppercase);
+
+// const a = 30;
+
+// if (a % 3 === 0 && a % 5 === 0) {
+//   console.log("Fizzbuzz");
+// } else if (a % 3 === 0) {
+//   console.log("Fizz");
+// } else if (a % 5 === 0) {
+//   console.log("Buzz");
+// }
+
+// const a = "Hello";
+// const b = "World!";
+
+// console.log(`${a}, ${b}`);
+
+// const q = 10;
+// const p = 6;
+// const average = (p + q) / 2;
+
+// console.log("Average", average);
+
+// const a = 0;
+
+// if (a > 0) {
+//   console.log("PositiveNumber");
+// } else if (a < 0) {
+//   console.log("NegativeNumber");
+// } else if (a === 0) {
+//   console.log("Number is Zero");
+// }
+
+// const a = 29;
+
+// if (a > 10) {
+//   console.log("10-aas ih");
+// } else {
+//   console.log("10-aas baga");
+// }
+
+const a = 10;
+const b = 11;
+
+if (a === b) {
+  console.log("Tentsuu");
+} else {
+  console.log("Ylgaatai");
+}
