8bf5d3afa2fb84c2d52d2d21510bdef74252448d
cb4yrra <t.chinbayr@gmail.com>
2025-11-20 12:12:42 +0800

    test

--- exercise 3/script.js
+++ exercise 3/script.js
@@ -1,105 +1,217 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
 
-console.log(fruits.shift(), fruits.pop());
+// console.log(fruits.shift(), fruits.pop());
 
-const fruits2 = ["Apple", "Banana"];
-fruits2.push("Mango");
+// const fruits2 = ["Apple", "Banana"];
+// fruits2.push("Mango");
 
-console.log(fruits2);
+// console.log(fruits2);
 
-const numbers = [1, 2, 3, 4];
-numbers.pop();
+// const numbers = [1, 2, 3, 4];
+// numbers.pop();
 
-console.log(numbers);
+// console.log(numbers);
 
-const number = [10, 20, 30];
-number.unshift("5");
+// const number = [10, 20, 30];
+// number.unshift("5");
 
-console.log(number);
+// console.log(number);
 
-const number2 = [100, 200, 300];
-number2.shift();
+// const number2 = [100, 200, 300];
+// number2.shift();
 
-console.log(number2);
+// console.log(number2);
 
-const num1 = [1, 2, 3];
-const num2 = [4, 5, 6];
+// const num1 = [1, 2, 3];
+// const num2 = [4, 5, 6];
 
-console.log(num1.concat(num2));
+// console.log(num1.concat(num2));
 
-const fruits3 = ["apple", "banana", "orange"];
-Includes = fruits3.includes("apple");
+// const fruits3 = ["apple", "banana", "orange"];
+// Includes = fruits3.includes("apple");
 
-console.log(Includes);
+// console.log(Includes);
 
-const animal = ["cat ", "dog", "bird"];
-IndexOf = animal.indexOf("dog") + 1;
+// const animal = ["cat ", "dog", "bird"];
+// IndexOf = animal.indexOf("dog") + 1;
 
-console.log("Dog is in", IndexOf);
+// console.log("Dog is in", IndexOf);
 
-const word = ["I", "Love", "JavaScript"];
-Join = word.join("-");
+// const word = ["I", "Love", "JavaScript"];
+// Join = word.join("-");
 
-console.log(Join);
+// console.log(Join);
 
-const num3 = [10, 20, 30, 40, 50];
-slicednum = num3.slice(0, 3);
+// const num3 = [10, 20, 30, 40, 50];
+// slicednum = num3.slice(0, 3);
 
-console.log(slicednum);
+// console.log(slicednum);
 
-const num4 = [1, 2, 3, 4];
-splicednum = num4.toSpliced(1, 1);
+// const num4 = [1, 2, 3, 4];
+// splicednum = num4.toSpliced(1, 1);
 
-console.log(splicednum);
+// console.log(splicednum);
 
-const num5 = [1, 2, 3, 4, 5];
-Reverse = num5.reverse();
+// const num5 = [1, 2, 3, 4, 5];
+// Reverse = num5.reverse();
 
-console.log(Reverse);
+// console.log(Reverse);
 
-const fruit = ["banana", "apple", "cherry"];
-Sort = fruit.sort();
+// const fruit = ["banana", "apple", "cherry"];
+// Sort = fruit.sort();
 
-console.log(Sort);
+// console.log(Sort);
 
-const array = ["a", "b", "c", "d"];
-ArraIs = Array.isArray(array);
+// const array = ["a", "b", "c", "d"];
+// ArraIs = Array.isArray(array);
 
-console.log(ArraIs);
+// console.log(ArraIs);
 
-const color = ["red", "blue", "green"];
-Strings = color.toString();
-Length = Strings.length;
+// const color = ["red", "blue", "green"];
+// Strings = color.toString();
+// Length = Strings.length;
 
-console.log(Length);
+// console.log(Length);
 
-const number3 = [10, 20, 30];
-Splice = number3.toSpliced(1, 1, "25");
+// const number3 = [10, 20, 30];
+// Splice = number3.toSpliced(1, 1, "25");
 
-console.log(Splice);
+// console.log(Splice);
 
-const lang = ["HTML", "CSS"];
-const lang1 = ["JavaScript", "React"];
-merge = lang.concat(lang1);
-elements = merge.length;
+// const lang = ["HTML", "CSS"];
+// const lang1 = ["JavaScript", "React"];
+// merge = lang.concat(lang1);
+// elements = merge.length;
 
-console.log(elements);
+// console.log(elements);
 
-const number4 = [1, 2, 3, 4, 5];
-splicedNumber = number4.toSpliced(3, 5);
+// const number4 = [1, 2, 3, 4, 5];
+// splicedNumber = number4.toSpliced(3, 5);
 
-console.log(splicedNumber);
+// console.log(splicedNumber);
 
-const num6 = [5, 10, 15, 20];
-numSplice = num6.toSpliced(2, 1);
+// const num6 = [5, 10, 15, 20];
+// numSplice = num6.toSpliced(2, 1);
 
-console.log(numSplice);
+// console.log(numSplice);
 
-const number5 = [100, 200, 300];
-Check = number5.includes(200);
+// const number5 = [100, 200, 300];
+// Check = number5.includes(200);
 
-if (Check === true) {
-  console.log("200 Байна.");
-} else {
-  console.log("200 Байхгүй.");
-}
+// if (Check === true) {
+//   console.log("200 Байна.");
+// } else {
+//   console.log("200 Байхгүй.");
+// }
+
+// let x = 0;
+
+// while (x < 1) {
+//   console.log("Pinecone");
+//   x++;
+// }
+
+// let i = 0;
+
+// while (i < 10) {
+//   console.log("Pinecone");
+//   i++;
+// }
+
+// let y = 0;
+// let n = 20;
+
+// while (y < n) {
+//   console.log("Pinecone");
+//   y++;
+// }
+
+// let i = 0;
+
+// while (i < 21) {
+//   if (i % 2 !== 0) {
+//     console.log(i, "Sondgoi Too");
+//   }
+//   i++;
+// }
+
+// let i = 0;
+
+// while (i < 41) {
+//   if (i % 2 == 0) {
+//     console.log(i, "Tegsh Too");
+//   }
+//   i++;
+// }
+
+// let i = 0;
+// result = 0;
+
+// // while (i <= 20) {
+// //   const niilber = i % 2 == 0;
+// //   if (niilber) {
+// //     result += i;
+// //   }
+// //   i++;
+// // }
+
+// console.log(result);
+
+// while (i <= 40) {
+//   const nillber = i % 2 !== 0;
+//   if (nillber) {
+//     result += i;
+//   }
+//   i++;
+// }
+
+// console.log(result);
+
+// let i = 0;
+// result = 0;
+// const n = 69;
+
+// while (i <= n) {
+//   result += i;
+//   i++;
+// }
+// console.log(result);
+
+// let i = 0;
+// result = 0;
+
+// while (i <= 10) {
+//   result += i * 20;
+//   i++;
+// }
+// console.log(result);
+
+// let i = 1;
+// result = 1;
+
+// while (i <= 125) {
+//   result *= i;
+//   i++;
+// }
+// console.log(result);
+
+// let i = 1;
+// result = 1;
+
+// while (i <= 125) {
+//   if (i % 2 !== 0) {
+//     result *= i;
+//   }
+//   i++;
+// }
+// console.log(result);
+
+// let i = 1;
+// result = 1;
+// n = 10;
+
+// while (i <= n) {
+//   result = result * i - 1;
+//   i++;
+// }
+// console.log(result);
